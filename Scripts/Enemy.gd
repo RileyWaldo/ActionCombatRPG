10c37515562ed54f2f9fb783b6991ece83d9ce88
Riley <riley_odlaw@hotmail.com>
2025-12-27 16:44:10 -0700

    Added enemy player detector

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -5,6 +5,8 @@ class_name Enemy
 
 @onready var healthComponent: HealthComponent = $HealthComponent
 @onready var rig: Rig = $Rig
+@onready var collisionShape: CollisionShape3D = $CollisionShape3D
+@onready var playerDetector: ShapeCast3D = $PlayerDetector
 @onready var meshes: Array[Node3D] = [
 	$Rig/CharacterRig/GameRig/Skeleton3D/Villager_01,
 	$Rig/CharacterRig/GameRig/Skeleton3D/Villager_02
@@ -13,7 +15,18 @@ class_name Enemy
 func _ready() -> void:
 	rig.SetRigCharacterMesh(meshes.pick_random())
 	healthComponent.UpdateMaxHealth(maxHealth)
+	
 
+func _physics_process(delta: float) -> void:
+	if(rig.IsIdle()):
+		CheckForAttacks()
+
+func CheckForAttacks() -> void:
+	for collision in playerDetector.get_collision_count():
+		var collider = playerDetector.get_collider(collision)
+		print(collider)
 
 func OnDefeat() -> void:
 	rig.Travel("Defeat")
+	collisionShape.disabled = true
+	set_physics_process(false)
