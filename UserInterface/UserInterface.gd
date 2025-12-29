0df4db606d9bf2b979b0cd21276d6250133d7f1a
Riley <riley_odlaw@hotmail.com>
2025-12-28 19:06:16 -0700

    Started Inventory menu

--- UserInterface/UserInterface.gd
+++ UserInterface/UserInterface.gd
@@ -7,6 +7,7 @@ class_name UserInterface
 @onready var healthbar: TextureProgressBar = %Healthbar
 @onready var xpBar: TextureProgressBar = %XPBar
 @onready var healthLabel: Label = %HealthLabel
+@onready var inventory: Control = $Inventory
 
 func UpdateStatsDisplay() -> void:
 	levelLabel.text = str(player.stats.level)
@@ -17,3 +18,6 @@ func UpdateHealth() -> void:
 	healthbar.max_value = player.healthComponent.maxHealth
 	healthbar.value = player.healthComponent.health
 	healthLabel.text = player.healthComponent.GetHealthString()
+
+func ToggleMenu() -> void:
+	inventory.visible = !inventory.visible
